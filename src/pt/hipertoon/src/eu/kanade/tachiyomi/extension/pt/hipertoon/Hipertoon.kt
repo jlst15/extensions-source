5540fc00805ae79caad4815151f411873c9841f3
jlst15 <jlst15@users.noreply.github.com>
2026-09-07 23:20:14 +0900

    extensions: isolate for KO extensions
    - this can be reverted by removing this   commit

--- src/pt/hipertoon/src/eu/kanade/tachiyomi/extension/pt/hipertoon/Hipertoon.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.pt.hipertoon
-
-import eu.kanade.tachiyomi.multisrc.hiper.Hiper
-import keiyoushi.annotation.Source
-
-@Source
-abstract class Hipertoon : Hiper()
